dab1107bf4e7360821c815950c71ed3d5ce14fa9
Samuel Ramos <samuelvanguarda@gmail.com>
2024-07-02 13:09:22 -0300

    FEAT: create functionality to remove task

--- js/script.js
+++ js/script.js
@@ -4,18 +4,34 @@ const addTask = () => {
   const taskTitle = document.getElementById('task-title').value;
 
   if (taskTitle) {
+    // clona template
     const template = document.querySelector('.template');
 
     const newTask = template.cloneNode(true);
 
+    // adiciona título
     newTask.querySelector('.task-title').textContent = taskTitle;
 
+    // remove as classes desnecessárias
     newTask.classList.remove('template');
     newTask.classList.remove('hide');
 
+    // adiciona tarefa na lista
     const list = document.getElementById('task-list');
     list.appendChild(newTask);
+
+    // adiciona evento de remover
+    const removeBtn = newTask.querySelector('.remove-btn').addEventListener('click', () => {
+      removeTask(this);
+    })
+
+    // limpa texto
+    document.querySelector('#task-title').value = '';
   }
+}
+
+const removeTask = (task) => {
+  task.parentNode(task);
 }
 
 addBtn.addEventListener('click', (event) => {
